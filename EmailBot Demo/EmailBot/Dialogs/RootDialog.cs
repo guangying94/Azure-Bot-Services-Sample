375c2a22f94575b75ce7914c183028b9af57cac8
Tee Guang Ying Marcus <guangyingtgy@live.com>
2017-09-23 21:31:09 +0800

    Added Email Bot
    Bot that will email everything to a specific email address.

---
+++ EmailBot Demo/EmailBot/Dialogs/RootDialog.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Net;
+using System.Net.Mail;
+
+namespace EmailBot.Dialogs
+{
+    [Serializable]
+    public class RootDialog : IDialog<object>
+    {
+        public Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+
+            return Task.CompletedTask;
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activity = await result as Activity;
+
+            SmtpClient client = new SmtpClient("smtp.live.com", 25);
+            client.EnableSsl = true;
+            client.Credentials = new NetworkCredential("<Sender Email>", "<Sender Password>");
+            try
+            {
+                client.Send("<sender email>", "<recipient email>", "<subject>", activity.Text);
+                await context.PostAsync("email sent!");
+            }
+            catch (Exception ex)
+            {
+                await context.PostAsync(ex.Message);
+            }
+
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
---
+++ EmailBot Demo/EmailBot/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EmailBot.WebApiApplication" Language="C#" %>
